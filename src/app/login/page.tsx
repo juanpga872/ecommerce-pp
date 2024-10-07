d56fbfd27293c61eaec4f9a5dc561bc0fece1bc0
juan pablo garces <juanpagacevedo@gmail.com>
2024-10-07 09:15:04 -0500

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,74 @@
+"use client";
 
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    username: "", 
+    password: ""
+  });
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const result = await signIn("credentials", {
+      redirect: false, 
+      username: formData.username, 
+      password: formData.password
+    });
+
+    if (result?.error) {
+      setError("Invalid login credentials");
+    } else if (result?.ok) {
+      // Obtener el token de la sesión después del login exitoso
+      const session = await fetch("/api/auth/session").then((res) => res.json());
+
+      // Guardar el token
+      if (session?.accessToken) {
+        sessionStorage.setItem("token", session.accessToken);
+      }
+      router.push("/");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username" 
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default LoginPage;
 
 
 
